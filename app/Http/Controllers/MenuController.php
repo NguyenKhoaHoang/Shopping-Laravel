a3f3281c53633bd88231a5bcda9126810497b42e
Nguyễn Khoa Hoàng <blcm2486@gmail.com>
2022-07-04 17:49:25 +0700

    add slug to menus

--- app/Http/Controllers/MenuController.php
+++ app/Http/Controllers/MenuController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use App\Components\MenuRecusive;
 use App\Models\Menu;
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Str;
 class MenuController extends Controller
 {
     private $menuRecusive;
@@ -30,7 +30,8 @@ class MenuController extends Controller
     {
         $this->menu->create([
             'name'=>$request->name,
-            'parent_id'=>$request->parent_id
+            'parent_id'=>$request->parent_id,
+            'slug'=>Str::slug($request->name)
         ]);
         return redirect()->route('menus.index');
     }
