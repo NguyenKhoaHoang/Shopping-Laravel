97b72b183e47265f51dca8436427b753343dc18f
Nguyễn Khoa Hoàng <blcm2486@gmail.com>
2022-07-04 17:33:16 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,68 @@
+<?php
+
+use App\Models\Category;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/home', function () {
+    return view('home');
+});
+
+Route::prefix('categories')->group(function () {
+    Route::get('/', [
+        'as' => 'categories.index',
+        'uses' => 'App\Http\Controllers\CategoryController@index'
+    ]);
+
+    Route::get('/create', [
+        'as' => 'categories.create',
+        'uses' => 'App\Http\Controllers\CategoryController@create'
+    ]);
+    Route::post('/store', [
+        'as' => 'categories.store',
+        'uses' => 'App\Http\Controllers\CategoryController@store'
+    ]);
+    Route::get('/edit/{id}', [
+        'as' => 'categories.edit',
+        'uses' => 'App\Http\Controllers\CategoryController@edit'
+    ]);
+    Route::post('/update/{id}', [
+        'as' => 'categories.update',
+        'uses' => 'App\Http\Controllers\CategoryController@update'
+    ]);
+    Route::get('/delete/{id}', [
+        'as' => 'categories.delete',
+        'uses' => 'App\Http\Controllers\CategoryController@delete'
+    ]);
+});
+
+Route::prefix('menus')->group(function () {
+    Route::get('/', [
+        'as' => 'menus.index',
+        'uses' => 'App\Http\Controllers\MenuController@index'
+    ]);
+
+    Route::get('/create', [
+        'as' => 'menus.create',
+        'uses' => 'App\Http\Controllers\MenuController@create'
+    ]);
+
+    Route::post('/store', [
+        'as' => 'menus.store',
+        'uses' => 'App\Http\Controllers\MenuController@store'
+    ]);
+});
